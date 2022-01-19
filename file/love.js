f7a5cb2249ea8fca37b8bbdb01ed08854990a823
李坤01 <likun01@qianxin.com>
2022-01-19 18:07:18 +0800

    fix: 背景色问题

--- file/love.js
+++ file/love.js
@@ -237,7 +237,7 @@
             var x = seed.x || this.width / 2;
             var y = seed.y || this.height / 2;
             var point = new Point(x, y);
-            var color = seed.color || '#FF0000';
+            var color = seed.color || '#FFFFFF';
             var scale = seed.scale || 1;
 
             this.seed = new Seed(this, point, scale, color);
